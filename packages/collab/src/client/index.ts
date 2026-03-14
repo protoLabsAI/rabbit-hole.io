337067d1fc5314c6ee2620c984078bd2ec569b74
Josh Mabry <mabry1985@gmail.com>
2026-03-13 17:04:00 -0700

    feat: Remove Hocuspocus and force local Yjs
    Implemented by Automaker auto-mode
    Feature ID: feature-1773445849323-t1f69gk3s

--- packages/collab/src/client/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @proto/collab/client - Client-side collaboration components
- *
- * React components for embedding Jitsi Meet in the application.
- */
-
-export * from "./JitsiMeet";
-export * from "./useCollaboration";
